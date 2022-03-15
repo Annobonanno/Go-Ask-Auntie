47ea2f7c4e86e58544b792cca0495558dca41178
Zack Ogilvie <zack@vincentdesign.ca>
2022-03-15 09:31:44 -0500

    nav,footer,temp-instagram

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{md,njk,html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+
+        navy: { DEFAULT : '#262259'},
+        red: { DEFAULT : '#EF3F5B'},
+        teal: { DEFAULT : '#3FBDAF',
+                'light': '#81C6CE',},
+        
+        purple: { DEFAULT : '#822369'},
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
